d2d7bf2528f4daac6ac9ea07f9b7b7552ab33797
DempeZheng <752996510@qq.com>
2015-10-19 16:42:28 +0800

    init

---
+++ src/main/java/com/yy/ent/srv/protocol/YYRequest.java
@@ -0,0 +1,20 @@
+package com.yy.ent.srv.protocol;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dempe
+ * Date: 2015/10/19
+ * Time: 16:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class YYRequest implements YYProto {
+    @Override
+    public long getUri() {
+        return 0;
+    }
+
+    @Override
+    public int getCode() {
+        return 0;
+    }
+}
