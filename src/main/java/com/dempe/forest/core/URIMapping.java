006a45aaa1ff5546c93ba47e647054bda9e57ece
dempeZheng <752996510@qq.com>
2016-11-28 17:20:17 +0800

    init

--- src/main/java/com/dempe/forest/core/URIMapping.java
+++ src/main/java/com/dempe/forest/core/URIMapping.java
@@ -2,7 +2,13 @@ package com.dempe.forest.core;
 
 import com.dempe.forest.core.invoker.InvokerWrapper;
 import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -12,16 +18,27 @@ import java.util.Map;
  * Time: 9:59
  * To change this template use File | Settings | File Templates.
  */
-public class URIMapping {
+public class URIMapping  implements ApplicationListener<ContextRefreshedEvent> {
 
+    @Value("base.package")
+    private String basePackage;
 
     private Map<Short, InvokerWrapper> mapping = Maps.newConcurrentMap();
 
     public void initMapping() {
+        // init mapping
 
     }
 
     public InvokerWrapper getInvokerWrapperByURI(short uri) {
         return mapping.get(uri);
     }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+       // spring 容器初始化完成后调用
+         initMapping();
+    }
+
 }
