c604cb1bf14580dc90eba452dbdec6ba25644e0b
dempeZheng <752996510@qq.com>
2016-12-15 10:10:02 +0800

    fix bug

--- forest-rpc/src/main/java/com/zhizus/forest/client/proxy/processor/ServiceExportProcessor.java
+++ forest-rpc/src/main/java/com/zhizus/forest/client/proxy/processor/ServiceExportProcessor.java
@@ -1,5 +1,6 @@
 package com.zhizus.forest.client.proxy.processor;
 
+import com.google.common.base.Strings;
 import com.zhizus.forest.common.annotation.MethodProvider;
 import com.zhizus.forest.common.annotation.ServiceExport;
 import com.zhizus.forest.common.annotation.ServiceProvider;
@@ -25,22 +26,33 @@ public class ServiceExportProcessor extends AbstractAnnotationProcessor {
         String methodName = null;
         for (Class<?> anInterface : interfaces) {
             ServiceProvider provider = anInterface.getAnnotation(ServiceProvider.class);
-            serviceName = provider.serviceName();
+            if (provider == null) {
+                continue;
+            }
+            serviceName = Strings.isNullOrEmpty(provider.serviceName()) ? serviceClass.getSimpleName() :
+                    provider.serviceName();
+
+            config.setServiceName(serviceName);
+            config.setPort(serviceExport.port());
+
             for (Method interfaceMethod : anInterface.getMethods()) {
+                MethodProvider methodProvider = interfaceMethod.getAnnotation(MethodProvider.class);
+                if (methodProvider == null) {
+                    continue;
+                }
                 if (methodEquals(interfaceMethod, method)) {
-                    MethodProvider methodProvider = interfaceMethod.getAnnotation(MethodProvider.class);
-                    if (methodProvider == null) {
-                        continue;
-                    }
-                    methodName = methodProvider.methodName();
+                    methodName = Strings.isNullOrEmpty(methodProvider.methodName()) ?
+                            interfaceMethod.getName() : methodProvider.methodName();
+
+                    MethodExportConfig methodExportConfig = new MethodExportConfig();
+                    methodExportConfig.setMethodName(methodName);
+                    config.register(method, methodExportConfig);
+
                 }
             }
         }
-        config.setServiceName(serviceName);
-        config.setPort(serviceExport.port());
-        MethodExportConfig methodExportConfig = new MethodExportConfig();
-        methodExportConfig.setMethodName(methodName);
-        config.register(method, methodExportConfig);
+
+
     }
 
 
